766e81d80f3338e7a827e4e2b2a31dc86e0f4179
Ben Metzger <bmetzger97@gmail.com>
2019-04-16 00:49:07 -0500

    Continuing work on login page

--- src/cgi-bin/login.py
+++ src/cgi-bin/login.py
@@ -14,4 +14,7 @@ import cgitb
 cgitb.enable() #enable error logging in browser, we probably want this on for a while
 # cgitb.enable(display=0, logdir="/path/to/logdir") # if you prefer log files
 
+print("Content-Type: text/html\n\n") #required header line
+print("working!")
+
 
