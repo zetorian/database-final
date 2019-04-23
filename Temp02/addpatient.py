620908211e33c98ac15fa8b28ca9b66dc93dac71
Seth Stoltenow <seth.stoltenow@gmail.com>
2019-04-22 22:30:57 -0500

    Updates to add patient
    Next work on the last query for the page

--- Temp02/addpatient.py
+++ Temp02/addpatient.py
@@ -8,8 +8,9 @@ import cgi,cgitb
 cnx = mysql.connector.connect(user='root',password='newpassword',host='localhost')
 cursor = cnx.cursor(buffered=True)
 
-#query="use EMR"
-#cursor.execute(query)
+args = ""
+query="use EMR"
+cursor.execute(query)
 
 #query = "INSERT INTO Patient (SSN, FName, LName, Phone, Address, InsuaranceCo, PrimaryDoctor) VALUES (%s, %s, %s, %s, %s, %s, %s);"
 #args = ""
@@ -20,32 +21,72 @@ form = cgi.FieldStorage()
 
 isPosted = form.getvalue('isposted')
 
-fname = form.getvalue('fname')
-lname = form.getvalue('lname')
-SSN = form.getvalue('ssn')
-phone = form.getvalue('phone')
-addr = form.getvalue('addr')
-insurance = form.getvalue('insurance')
-doc = form.getvalue('doc')
+
 
 fnameErrorMsg = ("Not a valid name")
 lnameErrorMsg = ("Invalid Last Name")
 SSNErrorMsg = ("Invalid SSN")
 SSNTakenMsg = ("Patient already exists with that SSN")
-PhoneErrorMsg = ("Invalid phone number")
+phoneErrorMsg = ("Invalid phone number")
 addrErrorMsg = ("Please enter a valid address")
 insuranceErrorMsg = ("Not a valid insurance company")
 docErrorMsg = ("Doctor not registered in this system")
 
 isError = False
 
-if fname is None or lname is None or SSN is None or phone is None or addr is None or insurance is None or doc is None:
-    isError = True
+ssnTaken = False
+
+
 
 if isPosted:
-    if isError:
-        ssnTaken = False
+
+    fname = form.getvalue('fname')
+    lname = form.getvalue('lname')
+    SSN = form.getvalue('ssn')
+    phone = form.getvalue('phone')
+    addr = form.getvalue('addr')
+    insurance = form.getvalue('insurance')
+    doc = form.getvalue('doc')
+
+    drSSN = None
+
+    if SSN is not None:
         
+        query ="SELECT * FROM Patient WHERE SSN =" + SSN + ";"
+        #query = "SELECT name FROM hospital;"
+        args = (SSN)
+        cursor.execute(query)
+        #records = cursor.fetchall()
+    
+    if cursor.rowcount != 0:
+        ssnTaken = True
+        isError = True
+
+    if doc is not None:
+        docSplit = doc.split()
+        if len(docSplit) != 2:
+            drSSN = None
+        else:
+            doctorFirst = docSplit[0]
+            doctorLast = docSplit[1]
+            doctorFirst.lower()
+            doctorLast.lower()
+            query ="SELECT SSN FROM Doctor WHERE fname ='" + doctorFirst + "' AND lname = '" + doctorLast + "';"
+            cursor.execute(query)
+
+            if cursor.rowcount == 0:
+                drSSN = None
+            else:
+                doctorReturn = cursor.fetchone()
+                drSSN = doctorReturn[0]
+
+    
+    if drSSN is None or fname is None or lname is None or SSN is None or phone is None or addr is None or insurance is None or doc is None:
+        isError = True
+    
+    if isError:
+       
+        #SSN = "100000030"
         print ("Content-type:text/html\r\n\r\n")
         print ('<html>')
         print ('<head>')
@@ -54,6 +95,13 @@ if isPosted:
         print ('</head>')
         print ('<body>')
         print ('<h2>Add Patient</h2>')
+
+        #print("Rows: " + str(cursor.rowcount) + "<br>") #get rid of this later
+        #print("SSN: " + SSN + "<br>")
+        #print("Query: " + query + "<br>")
+        #print("Args: " + args + "<br>")
+        if drSSN is not None:
+            print("Doctor: " + str(drSSN) + "<br>")
 
         print('<h3>')
         print("Name of the user is: %s" % fname)
@@ -122,7 +170,16 @@ if isPosted:
         print('Phone Number')
         print('</td>')
         print('<td>')
-        print('<input type="text" name="phone"></input>')
+        if phone:
+            print('<input type="text" name="phone" value="%s"></input>' % phone)
+        else:
+            print('<input type="text" name="phone"></input>')
+        print('</td>')
+        print('<td>')
+        if phone is None:
+            print('<p class="error">')
+            print(phoneErrorMsg)
+            print('</p>')
         print('</td>')
         print('</tr>')
 
@@ -131,7 +188,16 @@ if isPosted:
         print('Street Address')
         print('</td>')
         print('<td>')
-        print('<input type="text" name="addr"></input>')
+        if addr:
+            print('<input type="text" name="addr" value="%s"></input>' % addr)
+        else:
+            print('<input type="text" name="addr"></input>')
+        print('</td>')
+        print('<td>')
+        if addr is None:
+            print('<p class="error">')
+            print(addrErrorMsg)
+            print('</p>')
         print('</td>')
         print('</tr>')
 
@@ -139,9 +205,18 @@ if isPosted:
         print('<td>')
         print('Insurance Company')
         print('</td>')
-        print('<td>')
         #look up and cast to int
-        print('<input type="text" name="insurance"></input>')
+        print('<td>')
+        if insurance:
+            print('<input type="text" name="insurance" value="%s"></input>' % insurance)
+        else:
+            print('<input type="text" name="insurance"></input>')
+        print('</td>')
+        print('<td>')
+        if insurance is None:
+            print('<p class="error">')
+            print(insuranceErrorMsg)
+            print('</p>')
         print('</td>')
         print('</tr>')
 
@@ -149,9 +224,18 @@ if isPosted:
         print('<td>')
         print('Primary Doctor')
         print('</td>')
+        #look up and cast to social
         print('<td>')
-        #look up and cast as social security number
-        print('<input type="text" name="doc"></input>')
+        if doc:
+            print('<input type="text" name="doc" value="%s"></input>' % doc)
+        else:
+            print('<input type="text" name="doc"></input>')
+        print('</td>')
+        print('<td>')
+        if doc is None or drSSN is None:
+            print('<p class="error">')
+            print(docErrorMsg)
+            print('</p>')
         print('</td>')
         print('</tr>')
 
@@ -161,8 +245,15 @@ if isPosted:
         print('<input type="hidden" name="isposted" value="true"></input>')
         print('<input type="submit" value="Submit"></input>')
         print('</form>')
+        if ssnTaken:
+            print('<br>SSN taken<br>')
+        else:
+            print('<br>SSN not taken<br>')
         print ('</body>')
         print ('</html>')
+    #else:   #Here we store the results and tell the user we accepted the input
+        #query = "INSERT INTO patient (ssn, fname, lname, phone, address, insuranceCo, primaryDoctor) VALUES ('" + SSN + "', '" + fname + "','" + lname + "','" + phone + "','" + addr + "','" + insurance + "','" + drSSN + "';"
+
 
 else:
     print ("Content-type:text/html\r\n\r\n")
@@ -202,7 +293,7 @@ else:
     print('Social Security Number')
     print('</td>')
     print('<td>')
-    print('<input type="text" name="SSN"></input>')
+    print('<input type="text" name="ssn"></input>')
     print('</td>')
     print('</tr>')
 
@@ -252,3 +343,7 @@ else:
     print('</form>')
     print ('</body>')
     print ('</html>')
+
+cnx.commit()
+cursor.close()
+cnx.close()
