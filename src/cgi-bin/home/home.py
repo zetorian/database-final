f3db18e543a186db825dfaf51df7e4088972fee7
derekp8899 <38703627+derekp8899@users.noreply.github.com>
2019-04-25 13:08:45 -0500

    added in home page selector
    need to know how the values of user and user type should be set.

---
+++ src/cgi-bin/home/home.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python                        
+                                                                                                                                                                          
+import os                                                                                 
+import Cookie                                                                                                                                                             
+                                                                                                                                                                          
+print 'content-type: text/html\n'                                                                                                                                         
+                                                                                                                                                                          
+#here we need to set the user and type for the correct homepage of the logged in user                                                                                     
+                                                                                                                                                                          
+user=''                                                                                                                                                                   
+userType='doctor'                                                                                                                                                         
+                                                                                                                                                                          
+if userType=='doctor':                                                                                                                                                    
+    print '<head> <meta http-equiv="Refresh" content="1; url=homeDoc.py"> </head>'                                                                                        
+                                                                                                                                                                          
+if userType=='nurse':                                                                                                                                                     
+    print '<head> <meta http-equiv="Refresh" content="1; url=homeNurse.py"> </head>'                                                                                      
+                                                                                                                                                                          
+if userType=='patient':                                                                                                                                                   
+    print '<head> <meta http-equiv="Refresh" content="1; url=homePatient.py"> </head>'                                                                                    
+                                                                                                                                                                                                                                                                   
