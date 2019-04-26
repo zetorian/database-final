f4ca61b8fe0bf9016cce948cc649779e3a2ce5f3
Derek P <>
2019-04-26 10:13:26 -0500

    added in the table for doctor appointments

--- src/cgi-bin/home/homeDoc.py
+++ src/cgi-bin/home/homeDoc.py
@@ -19,10 +19,15 @@ print '<body>\n'
 
 cnx = mysql.connector.connect(user='root',password='',host='localhost')
 cursor = cnx.cursor(buffered=True)
+cursor2 = cnx.cursor(buffered=True)
 #print 'attempting query'
 cursor.execute('USE EMR;')
 query = 'select lname,fname,ssn from patient where primaryDoctor='+userSSN+';'
 cursor.execute(query)
+
+#
+# Table to display the doctor's patients
+#
 
 print '<table><caption>Your Patients</caption>'
 print '<thead>'
@@ -40,6 +45,40 @@ for lname,fname,ssn in cursor:
     print '<td>'+ fname+ '</td>'
     print '<td>'+ ssn+ '</td>'
     print '</tr>'
+
+print '</tbody>'
+
+#
+# Table to display the doctors appointments
+#
+
+print '<table><caption>Your Appointments</caption>'
+print '<thead>'
+print '<tr>'
+print '<th>Last Name </th>'
+print '<th>First Name </th>'
+print '<th>SSN </th>'
+print '<th>Date/Time </th>'
+print '<th>Location </th>'
+print '</tr>'
+print '</thead>'
+print '<tbody>'
+
+print '<br />'
+print '<br />'
+query = 'select dateTime, location, patient from Appointment where doctor ='+userSSN+ ';'
+
+for dateTime,location,patient in cursor:
+    cursor2.execute('SELECT lname, fname from patient where ssn ='+patient+';');
+    for lname, fname in cursor2:
+        
+        print '<tr>'
+        print '<td>'+ lname+ '</td>'
+        print '<td>'+ fname+ '</td>'
+        print '<td>'+ patient+ '</td>'
+        print '<td>'+ dateTime+ '</td>'
+        print '<td>'+ location+ '</td>'
+        print '</tr>'
 
 print '</tbody>'
 
