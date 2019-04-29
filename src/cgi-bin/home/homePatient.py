debd43dc95cd0398b167f5206b65e300c9862390
Derek P <>
2019-04-28 19:38:40 -0500

    added new queries to the patient home

--- src/cgi-bin/home/homePatient.py
+++ src/cgi-bin/home/homePatient.py
@@ -13,6 +13,7 @@ form = cgi.FieldStorage()
 patientSSN = form.getvalue('ssn')
 cnx = mysql.connector.connect(user='root',password='',host='localhost')
 cursor = cnx.cursor(buffered=True)
+cursor2 = cnx.cursor(buffered=True)
 cursor.execute("USE EMR;")
 
 print "Content-type:text/html\r\n\r\n"
@@ -30,7 +31,7 @@ print '</h1>\n'
 print '<body>\n'
 
 
-query='select lname,fname,ssn,address,phone from patient where ssn ='+userSSN+';'
+query='select lname,fname,ssn,address,phone,primaryDoctor from patient where ssn ='+userSSN+';'
 cursor.execute(query);
 
 print '<table><caption>Your Details</caption>'
@@ -41,17 +42,21 @@ print '<th>First Name </th>'
 print '<th>SSN </th>'
 print '<th>Address </th>'
 print '<th>Phone</th>'
+print '<th>Primary Doctor</th>'
 print '</tr>'
 print '</thead>'
 print '<tbody>'
 
-for lname, fname,ssn,address,phone in cursor:
+for lname, fname,ssn,address,phone,primaryDoctor in cursor:
     print '<tr>'
     print '<td>'+ lname+ '</td>'
     print '<td>'+ fname+ '</td>'
     print '<td>'+ ssn+ '</td>'
     print '<td>'+ address+ '</td>'
     print '<td>'+ phone+ '</td>'
+    cursor2.execute('select fname,lname from doctor where ssn ='+primaryDoctor+';')
+    for fname, lname in cursor2:
+        print '<th>'+fname+' ' +lname+'</th>'
     print '</tr>'
 
 print '</tbody>'
@@ -69,6 +74,17 @@ print '</tr>'
 print '</thead>'
 print '<tbody>'
 
+query='select dateTime,location,doctor from Appointment where patient ='+userSSN+';'
+cursor.execute(query)
+for doctor, in cursor:
+    cursor2.execute('select fname,lname from doctor where ssn ='+doctor+';')
+for dateTime,location in cursor:
+    print '<tr>'
+    print '<th>'+dateTime+'</th>'
+    print '<th>'+location+'</th>'
+    for fname,lname in cursor2:
+        print '<th>'+fname+' ' +lname+'</th>'
+    print '</tr>'
 print '</tbody>'
 
 print '<br />'
@@ -86,6 +102,17 @@ print '</tr>'
 print '</thead>'
 print '<tbody>'
 
+query='select dateTime,doctor,id,procedureName from procedures where patient ='+userSSN+';'
+cursor.execute(query);
+print '<tr>'
+for dateTime,doctor,id,procedureName in cursor:
+    print '<th>'+dateTime+'</th>'
+    print '<th>'+location+'</th>'
+    print '<th>'+doctor+'</th>'
+    print '<th>'+procedureName+'</th>'
+    print '<th>'+id+'</th>'
+print '</tr>'
+
 print '</tbody>'
 
 print '<br />'
@@ -97,13 +124,21 @@ print '<tr>'
 print '<th>Date</th>'
 print '<th>ID</th>'
 print '<th>Expires</th>'
-print '<th>Drug Name</th>'
 print '<th>Doctor</th>'
 print '</tr>'
 print '</thead>'
 print '<tbody>'
 
-
+query='select doctor,prescriptionNum,date,expires from presciption where patient ='+userSSN+';'
+cursor.execute(query);
+for doctor,prescriptionNum,date,expires in cursor:
+    print '<tr>'
+    print '<th>'+date+'</th>'
+    print '<th>'+prescriptionNum+'</th>'
+    print '<th>'+expires+'</th>'
+    print '<th>'+doctor+'</th>'
+print '</tr>'
+    
 print '</tbody>'
 
 print '<br />'
